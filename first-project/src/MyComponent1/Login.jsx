09ec9439120dafad94e86defbd2f351362daaed0
sagar-prasad-01 <sagarmadheshiya82@gmail.com>
2026-04-28 22:57:23 +0530

    login page

--- first-project/src/MyComponent1/Login.jsx
+++ first-project/src/MyComponent1/Login.jsx
@@ -1,38 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Login = () => {
+  [FormData, setFormData] = useState({
+    fullName: "",
+    userName: "",
+  });
   return (
-    <div className="min-h-screen flex items-center justify-center bg-gray-100">
-      
-      <div className="bg-white p-8 rounded-2xl shadow-lg w-[350px]">
-        
-        <h1 className="text-3xl font-bold text-center mb-6 text-gray-800">
-          Login
-        </h1>
-
-        <form className="flex flex-col gap-4">
-          
-          <input
-            type="email"
-            placeholder="Enter your email"
-            className="border border-gray-300 p-3 rounded-lg outline-none focus:ring-2 focus:ring-blue-400"
-          />
-
-          <input
-            type="password"
-            placeholder="Enter your password"
-            className="border border-gray-300 p-3 rounded-lg outline-none focus:ring-2 focus:ring-blue-400"
-          />
-
-          <button className="bg-blue-500 text-white p-3 rounded-lg hover:bg-blue-600 transition">
-            Login
-          </button>
-        </form>
-
-        <p className="text-center mt-4 text-sm text-gray-500">
-          Don't have an account? <span className="text-blue-500 cursor-pointer">Sign Up</span>
-        </p>
-      </div>
+    <div>
+      <label htmlFor="Fullname">Fullname</label>
+      <input
+        type="text"
+        placeholder="enter full name"
+        id="Fullname"
+        value={FormData.fullName}
+      />
+      <label htmlFor="userName">userName</label>
+      <input
+        type="text"
+        placeholder="enter full name"
+        id="userName"
+        value={FormData.userName}
+      />
     </div>
   );
 };
