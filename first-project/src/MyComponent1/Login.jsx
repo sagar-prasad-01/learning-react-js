f113c1cb460d792b500abfa1752517ca049f680a
sagar-prasad-01 <sagarmadheshiya82@gmail.com>
2026-04-30 23:49:00 +0530

    login

--- first-project/src/MyComponent1/Login.jsx
+++ first-project/src/MyComponent1/Login.jsx
@@ -1,26 +1,56 @@
-import React, { useState } from "react";
+import React from "react";
+import { useState } from "react";
 
 const Login = () => {
-  [FormData, setFormData] = useState({
+  let [formData, setFormData] = useState({
     fullName: "",
     userName: "",
   });
+
+  let handleInputChange = (event) => {
+    let fieldName = event.target.name;
+    let newValue = event.target.value;
+    console.log(fieldName);
+
+    setFormData((currData) => {
+      return {
+        ...currData,
+        [fieldName]: newValue,
+      };
+    });
+  };
+
+  let handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(formData);
+    setFormData({
+      fullName: "",
+      userName: "",
+    });
+  };
   return (
     <div>
-      <label htmlFor="Fullname">Fullname</label>
-      <input
-        type="text"
-        placeholder="enter full name"
-        id="Fullname"
-        value={FormData.fullName}
-      />
-      <label htmlFor="userName">userName</label>
-      <input
-        type="text"
-        placeholder="enter full name"
-        id="userName"
-        value={FormData.userName}
-      />
+      <form action="" onSubmit={handleSubmit}>
+        <label htmlFor="fullname">Fullname</label>
+        <input
+          type="text"
+          placeholder="enter full name"
+          id="fullname"
+          value={formData.fullName}
+          name="fullName"
+          onChange={handleInputChange}
+        />
+        <label htmlFor="userName">userName</label>
+        <input
+          type="text"
+          placeholder="enter full name"
+          id="userName"
+          value={formData.userName}
+          name="userName"
+          onChange={handleInputChange}
+        />
+        <button>submit</button>
+      </form>
     </div>
   );
 };
